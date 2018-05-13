2bece2589eb7869cee3ee3dbcfe8fd19f10b9b5d
amcolling <amcolling@gmail.com>
2018-05-13 12:28:03 -0600

    WIP

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,7 +8,11 @@ class Enigma
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
     "u", "v", "w", "x", "y", "z", "0", "1", "2", "3",
     "4", "5", "6", "7", "8", "9", " ", ".", ","]
-    
+
   end
+
+  def gears
+    Key.new 
+    Date.new
 
 end
