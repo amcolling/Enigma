5b3c45d10824ed5f0f54b47c9345a6dd077eb9d4
amcolling <amcolling@gmail.com>
2018-05-12 18:44:11 -0600

    created random key and key generator

--- lib/key.rb
+++ lib/key.rb
@@ -1,21 +1,23 @@
 require 'pry'
 class Key
-  attr_reader :key
+  attr_reader :abcd,
+              :rand_offsets
 
   def initialize
+    @abcd = ["a", "b", "c", "d"]
+    @rand_offsets = []
   end
 
   def key_generator
-    keys = [rand(1..9),rand(1..9),rand(1..9),rand(1..9),rand(1..9)]
+    (1..5).to_a.shuffle
   end
 
-
-
-
-
-
-
-
-
+  def random_key
+    @rand_offsets << key_generator[0..1].join.to_i
+    @rand_offsets << key_generator[1..2].join.to_i
+    @rand_offsets << key_generator[2..3].join.to_i
+    @rand_offsets << key_generator[3..4].join.to_i
+    @rand_offsets << key_generator[4..5].join_to_i
+  end
 
 end
