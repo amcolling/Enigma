4f071f6483c3825ecdfffc9d73afa4292df397ab
amcolling <amcolling@gmail.com>
2018-05-14 14:32:29 -0600

    WIP

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,19 +17,4 @@ class EnigmaTest < Minitest::Test
     result = ["t", "h", "i", "s", " ", "i", "s", " ", "s", "o", " ", "s", "e", "c", "r", "e", "t", " ", ".", ".", "e", "n", "d", ".", "."]
     assert_equal result, enigma.message
   end
-
-  # def test_rots_are_added_to_offsets
-  #   enigma = Enigma.new('message.txt')
-  #   key = KeyGenerator.new.rotations([1, 2, 3, 4, 5])
-  #   offsets = OffsetCalculator.new(Date.today, key)
-  #   assert_equal [19, 24, 36, 49], enigma.rots_added_to_offsets
-  # end
-
-  def test_rotation_cogs
-    enigma = Enigma.new('message.txt')
-    key = KeyGenerator.new.rotations([1, 2, 3, 4, 5])
-    offset = OffsetCalculator.new(Date.today, key)
-    assert_equal [], enigma.rotation_cogs(Date.today, key)
-  end
-
 end
