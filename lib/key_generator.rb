9d6d0eb8c129a66178de013a8e5e81cc0b1de1aa
JohnRoemer <jt.r@live.com>
2018-05-15 17:43:33 -0600

    refactoring key

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,17 +1,19 @@
-
 class KeyGenerator
-
-  def random_key
-    Array.new(5).map do |num|
-    rand(9)
-    end
-  end
-
-  def rotations(key = random_key)
-    rots = []
-    rots << key[0..1].join.to_i
-    rots << key[1..2].join.to_i
-    rots << key[2..3].join.to_i
-    rots << key[3..4].join.to_i
+  def create
+    "rand(10000..99999)"
   end
 end
+#   def random_key
+#     Array.new(5).map do |num|
+#     rand(9)
+#     end
+#   end
+#
+#   def rotations(key = random_key)
+#     rots = []
+#     rots << key[0..1].join.to_i
+#     rots << key[1..2].join.to_i
+#     rots << key[2..3].join.to_i
+#     rots << key[3..4].join.to_i
+#   end
+# end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,11 +8,17 @@ class KeyGeneratorTest < Minitest::Test
     assert_instance_of KeyGenerator, key
   end
 
-  def test_rotations
+  def test_it_creates_string_of_five_random_numbers
     key = KeyGenerator.new
-    assert_instance_of Array, key.rotations
-    assert_instance_of Integer, key.rotations[2]
+    assert_instance_of String, key.create
+    assert 5, key.create.length
+    
   end
+  # def test_rotations
+  #   key = KeyGenerator.new
+  #   assert_instance_of Array, key.rotations
+  #   assert_instance_of Integer, key.rotations[2]
+  # end
 
 
 end
