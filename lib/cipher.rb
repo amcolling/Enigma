4d4dc67e06381b7b64b2e430ef606931e535415f
amcolling <amcolling@gmail.com>
2018-05-15 22:06:15 -0600

    working on cipher class

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,16 +1,70 @@
 require 'pry'
 require 'date'
 class Cipher
-  attr_reader :message,
-              :key,
-              :date
+  attr_reader :a
 
-  def initialize(message, key = KeyGenerator.new, date = Date.today)
-    @message  = message
-    @key      = key
-    @date     = date
-    
+  def initialize
+    @key = KeyGenerator.new.create
+    @character_map =
+    [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z", "0", "1", "2", "3",
+    "4", "5", "6", "7", "8", "9", " ", ".", ","]
   end
+
+  def encrypt_single(my_message, key = "37766")
+    zipped_hash = @character_map.zip(@character_map.rotate(@key[0])).to_h
+    encrypted_letter = zipped_hash.fetch(my_message)
+    encrypted_letter
+  end
+
+  def decrypt_single(my_message, key, offset)
+    rots = rotations(key)
+    rotations = combine_rotations_and_offsets(rots, offset)
+    zipped_hash = @character_map.zip(@character_map.rotate(key_rotations[0]*-1)).to_h
+    encrypted_letter = zipped_hash.fetch(my_message)
+    encrypted_letter
+  end
+
+  def encrypt_message(my_message, key = @key, date = Date.today)
+    offset = OffsetCalculator.new(key, date)
+    offset.offsets
+    rots = rotations(key)
+
+    letters = my_message.to_s.downcase.split("")
+    count = 0
+    letters.map do |letter|
+      (count = 0) if count == 4
+      encrypted_letter = encrypt_single(letter, rots, offset[count])
+      count += 1
+      binding.pry
+      encrypted_letter
+    end.join
+  end
+
+  def decrypt_message(my_message, key = @key, offset)
+    rots = rotations(key)
+    my_message = my_message.to_s.downcase.split("")
+    count = 0
+    decryption = my_message.map do |letter|
+      (count = 0) if count == 4
+      encrypted_letter = decrypt_single(letter, rots, offset[count])
+      count += 1
+      encrypted_letter
+    end.join
+  end
+
+  def crack
+    key = ("9999").to_i
+    cracking = encrypt_message(message, key, date)
+    until cracking[-7..-1] == "..end.." do
+      cracking = encrypt_message(message,key,date)
+      key += 1
+      break if key == 99999
+    end
+    key -= 1
+  end
+end
 
 
 
@@ -32,5 +86,3 @@ class Cipher
   #   encrypted_letter = zipped_hash.fetch(my_message)
   #   encrypted_letter
   # end
-
-end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -1,15 +1,62 @@
 require './test/test_helper.rb'
 require './lib/key_generator'
 require './lib/offset_calculator'
-require './lib/enigma'
+# require './lib/enigma'
 require './lib/cipher'
-
-
-
 
 class CipherTest < Minitest::Test
   def test_it_exists
-    cipher = Cipher.new("this is so secret ..end..")
+    cipher = Cipher.new
     assert_instance_of Cipher, cipher
   end
-end
+  def test_encrypt_single
+    cipher = Cipher.new
+    key = "37766"
+    offset = [7,1,2,4]
+    assert_equal "y", cipher.encrypt_single("t")
+  end
+  def test_encrypt_message
+    skip
+    enigma = Enigma.new
+    key = "37766"
+    offset = [7,1,2,4]
+    assert_equal "sggq9hq8rn8qdbpcs999dmb9 ", enigma.encrypt_message("this is so secret ..end..")
+  end
+  def test_decrypt_single
+    skip
+    enigma = Enigma.new
+    key = "37766"
+    offset = [7,1,2,4]
+    assert_equal "t", enigma.decrypt_single("y", key, offset)
+  end
+  def test_decrypt_message
+    skip
+    enigma = Enigma.new
+    key = "37766"
+    offset = [7,1,2,4]
+    assert_equal "this is so secret ..end..", enigma.decrypt_message("sggq9hq8rn8qdbpcs999dmb9 ", key, offset)
+  end
+  def test_crack_works
+      skip
+
+      enigma = Enigma.new
+      cipher = Cipher.new(message, key = KeyGenerator.new, date = Date.today)
+      date = [7,1,2,4]
+      enigma.decrypt_message("sggq9hq8rn8qdbpcs999dmb9 ", key, offset)
+      assert_equal "this is so secret ..end..", enigma.crack
+      # ("sggq9hq8rn8qdbpcs999dmb9 ", cipher.date)
+  end
+
+
+
+  def test_turn_key_into_four_two_digit_value_array
+      skip
+      key = "37766"
+      enigma = Enigma.new
+      assert_equal [37,77,76,66], enigma.turn_key_into_four_two_digit_value_array(key)
+  end
+
+
+
+
+  end
