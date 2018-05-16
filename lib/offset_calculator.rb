5bb003389ed6b672e17988056d31ece1a197eff4
JohnRoemer <jt.r@live.com>
2018-05-15 20:47:32 -0600

    all tests are green on new offset_calculator

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -10,18 +10,45 @@ class OffsetCalculator
     @date = date
   end
 #if date is Date object it converts it to 6 digit style
-  def reformat_date
-    if @date.class == Date
-      @date.strftime("%e%m%y").to_i
+  def reformat_date(date)
+    if date.class == Date
+      date.strftime("%e%m%y").to_i
     else
-      @date
+      date
     end
   end
 #after converted, it squares the date
+  def square_the_date(date)
+    (date ** 2).to_s
+  end
 #after it squares the date, it takes the last four digits
+  def slice_into_four(date)
+    sliced = date.slice(-4..-1)
+    sliced
+  end
 #after it gets last four digits, it makes an array of those
+  def string_to_int_array(date)
+    array = date.chars
+    array.map(&:to_i)
+  end
 #then it makes a four digit array from the key
+  def key_rotations(key)
+    key_array = key.chars
+    rots = []
+    rots << key_array[0..1].join.to_i
+    rots << key_array[1..2].join.to_i
+    rots << key_array[2..3].join.to_i
+    rots << key_array[3..4].join.to_i
+  end
 #then it adds together array of date and array of key
+def create(key_array, date_array)
+  rotation_array = []
+  rotation_array << date_array[0] + key_array[0]
+  rotation_array << date_array[1] + key_array[1]
+  rotation_array << date_array[2] + key_array[2]
+  rotation_array << date_array[3] + key_array[3]
+  rotation_array
+end
 
 
 
--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -23,53 +23,61 @@ class OffsetCalculatorTest < Minitest::Test
 
   def test_it_reformats_date_if_date_is_date_object
     offset = OffsetCalculator.new("23567")
+    date = offset.date
+    assert_equal Date.today.strftime("%e%m%y").to_i, offset.reformat_date(date)
 
-    assert_equal Date.today.strftime("%e%m%y").to_i, offset.reformat_date
-
-    offset2 = OffsetCalculator.new("23567", 51518)
-    assert_equal 51518, offset2.reformat_date
+    offset2 = OffsetCalculator.new("23567", 150518)
+    date = offset2.date
+    assert_equal 150518, offset2.reformat_date(date)
   end
 
   def test_it_squares_the_date_and_changes_integer_to_string
-    skip
-    offset = OffsetCalculator.new(Date.today)
-    date_reformatted = 51318
-    assert_equal "2633537124", offset.squares_the_date(date_reformatted)
+    offset = OffsetCalculator.new("23567", 51318)
+    date = offset.date
+
+    assert_equal "2633537124", offset.square_the_date(date)
   end
 
   def test_slice_into_four
-    skip
-    offset = OffsetCalculator.new(123456)
-    date_squared = "2633537124"
-    actual = offset.slice_into_four(date_squared)
-    expected = "7124"
-    assert_equal expected, actual
+    offset = OffsetCalculator.new("23567", 51318)
+
+    assert_equal "7124", offset.slice_into_four("2633537124")
   end
 
   def test_it_turns_four_digit_string_into_array
-    skip
-    offset = OffsetCalculator.new(Date.today)
-    four_numbers = "7124"
-    actual = offset.string_to_int_array(four_numbers)
-    expected = [7, 1, 2, 4]
-    assert_equal expected, actual
+    offset = OffsetCalculator.new("23567", 51318)
+
+    assert_equal [7, 1, 2, 4], offset.string_to_int_array("7124")
+  end
+  def test_it_makes_four_value_array_from_joining_digits_from_string
+    offset = OffsetCalculator.new("23567", 51318)
+
+    assert_equal [23, 35, 56, 67], offset.key_rotations(offset.key)
   end
 
-  def test_it_generates_a_four_value_array_from_todays_date
-    skip
-    offset = OffsetCalculator.new(Date.today)
-    assert_instance_of Array, offset.generate
-    assert_equal 4, offset.generate.length
-    assert_instance_of Integer, offset.generate[1]
-    assert_equal  [4, 3, 2, 4], offset.generate
-  end
+  def test_create_offset
+    offset = OffsetCalculator.new("23567", 51318)
+    key_array  = [23, 35, 56, 67]
+    date_array = [7, 1, 2, 4]
 
-  def test_generate_date
-    skip
-    offset = OffsetCalculator.new(Date.today)
-    assert_instance_of Date, offset.date
-    assert_equal  [4, 3, 2, 4], offset.generate
+    assert_equal [30, 36, 58, 71], offset.create(key_array, date_array)
   end
+  
+  # def test_it_generates_a_four_value_array_from_todays_date
+  #   skip
+  #   offset = OffsetCalculator.new(Date.today)
+  #   assert_instance_of Array, offset.generate
+  #   assert_equal 4, offset.generate.length
+  #   assert_instance_of Integer, offset.generate[1]
+  #   assert_equal  [4, 3, 2, 4], offset.generate
+  # end
+  #
+  # def test_generate_date
+  #   skip
+  #   offset = OffsetCalculator.new(Date.today)
+  #   assert_instance_of Date, offset.date
+  #   assert_equal  [4, 3, 2, 4], offset.generate
+  # end
 
 
 end
