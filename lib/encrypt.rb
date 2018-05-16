49698a79c786bd91c68e7f08efc7ffcefcfdb69f
amcolling <amcolling@gmail.com>
2018-05-16 15:46:07 -0600

    reading, writing from encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,16 +1,15 @@
-
-require './lib/enigma'
-require './test/test_helper.rb'
 require './lib/key_generator'
 require './lib/offset_calculator'
 require './lib/enigma'
 require './lib/cipher'
 
 filename = File.open(ARGV[0], 'r')
-input_text = filename.read.gsub("\n", " ").strip
+input_text = filename.read
+
 filename.close
 
 e = Enigma.new
+
 
 output_file = File.open(ARGV[1], 'w')
 output_file.write(e.encrypt(input_text))
