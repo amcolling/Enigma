3004587072b84e83adce93faa392728d36a114fe
amcolling <amcolling@gmail.com>
2018-05-16 17:14:27 -0600

    finished, refactoring

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -15,19 +15,19 @@ class Enigma
   def encrypt(my_message, key = KeyGenerator.new.create, date = Date.today)
     @key = key
     @date = date
-    output = Cipher.new.encrypt_message(my_message, key, date)
+    Cipher.new.encrypt_message(my_message, key, date)
   end
 
   def decrypt(output, key, date = Date.today)
     @key = key
     @date = date
-    output = Cipher.new.decrypt_message(output, key, date)
+    Cipher.new.decrypt_message(output, key, date)
   end
 
   def crack(output, date = Date.today)
     @date = date
-    output = Cipher.new.crack(output, date)
-  end 
+    Cipher.new.crack(output, date)
+  end
 
 
 
